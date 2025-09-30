5fe27a03d492bc3e746ed557cad2afb464bc5d00
Nicolle2024 <nicolle.castilla@gmail.com>
2025-09-30 14:38:45 -0500

    Par.java

---
+++ Par.java
@@ -0,0 +1,25 @@
+import java.util.Objects;
+public class Par<F, S> {
+  private F primero;
+  private S segundo;
+
+  public Par(F primero, S segundo){
+    this.primero = primero;
+    this.segundo = segundo;
+  }
+  public F getPrimero() {
+    return primero;
+  }
+  public S getSegundo() {
+    return segundo;
+  }
+  public void setPrimero (F primero) {
+    this.primero = primero;
+  }
+  public void setSegundo (S segundo) {
+    this.segundo = segundo;
+  }
+  public String toString() {
+    return "(Primero: " + primero+ ", Segundo: " + segundo + ")"
+  }
+}
